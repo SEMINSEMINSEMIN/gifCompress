c9f9aac3408e5f7ba653708e3ff6253751002d9e
SeminKang <ksm9802@gmail.com>
2023-01-15 10:50:18 +0900

    Feat: gif에서 프레임 추출 후 다시 gif로 만들기
    이용 라이브러리
        1. gifler: gif에서 프레임 추출 위함
        2. gif.js.optimized: gif encoder, 프레임 추출한거 바탕으로 다시 gif를 만들기 위해
    한계점
        1. 서버에 이미지 등록이 제대로 되고 있지 않음: console.log(res.json) 결과가 빈 객체로 나온다
        2. gifler의 createBufferCanvas에 접근하는 방법을 모르겠어서 해당 코드를 직접 입력함(라이브러리를 제대로 활용하고 있지 않음)

---
+++ my-app/src/App.jsx
@@ -0,0 +1,100 @@
+// https://github.com/jnordberg/gif.js/issues/115
+import { useEffect, useRef, useState } from "react";
+import GIF from "gif.js.optimized";
+import "gifler";
+import QQ from "./assets/gif/run.gif";
+
+function App() {
+    const frameExtracted = useRef();
+    const [output, setOutput] = useState();
+
+    useEffect(() => {
+        const gif = new GIF({
+            workers: 2,
+            workerScript: process.env.PUBLIC_URL + "/gif.worker.js",
+            quality: 10,
+        });
+
+        window.gifler(QQ).get((a) => {
+            const res = a["_frames"].map((frame) => {
+                let bufferCanvas, bufferContext, imageData;
+                bufferCanvas = document.createElement("canvas");
+                bufferContext = bufferCanvas.getContext("2d");
+                bufferCanvas.width = frame.width;
+                bufferCanvas.height = frame.height;
+                imageData = bufferContext.createImageData(
+                    frame.width,
+                    frame.height
+                );
+                imageData.data.set(frame.pixels);
+                bufferContext.putImageData(imageData, -frame.x, -frame.y);
+                document.body.append(bufferCanvas);
+                return bufferContext.canvas.toDataURL();
+            });
+
+            frameExtracted.current = res;
+
+            const exportCuts = async () => {
+                const cuts = frameExtracted.current;
+
+                cuts.forEach((imgUrl) => {
+                    const img = new Image();
+                    img.src = imgUrl;
+                    img.width = 400;
+                    img.height = 400;
+                    gif.addFrame(img, { delay: 100 });
+                });
+
+                gif.on("finished", async (blob) => {
+                    console.log("blob:")
+                    console.log(blob);
+                    console.log("---------------");
+
+                    const url = URL.createObjectURL(blob);
+                    const file = new File([blob], "흑흑", {
+                        type: "image/gif",
+                    });
+
+                    console.log("blob to file:")
+                    console.log(file);
+                    console.log("---------------");
+                    setOutput(url);
+
+                    const formData = new FormData();
+                    formData.append("image", file);
+
+                    // https://velog.io/@josworks27/formData-console.log
+                    for (let value of formData.values()) {
+                        console.log("formdata에 file이 제대로 담겼는지 확인:")
+                        console.log(value);
+                        console.log("---------------");
+                    }
+
+                    const requestUrl = "https://mandarin.api.weniv.co.kr/";
+
+                    const res = await fetch(requestUrl + "image/uploadfile", {
+                        method: "POST",
+                        body: formData,
+                        headers: {
+                            "Content-type": "multipart/form-data",
+                        },
+                    });
+                    console.log("res:")
+                    console.log(res);
+                    console.log("---------------");
+
+                    const json = await res.json();
+                    console.log("res.json:")
+                    console.log(json);
+                });
+
+                gif.render();
+            };
+
+            exportCuts();
+        });
+    }, []);
+
+    return <img src={output} alt="rendering" />;
+}
+export default App;
