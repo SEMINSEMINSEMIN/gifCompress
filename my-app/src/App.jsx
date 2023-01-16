143b772b7b3e4b5f77c7f13b8b89b7bdcf3f2252
SeminKang <ksm9802@gmail.com>
2023-01-17 02:18:16 +0900

    Feat: gif 압축 기능 추가
    한계점: 투명 배경 처리 문제 - 투명 배경을 처리하려고 하면 gif가 하얗게 보이는 부분이 생기는 문제가 발생

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,70 +1,86 @@
 // https://github.com/jnordberg/gif.js/issues/115
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import GIF from "gif.js.optimized";
 import "gifler";
-import QQ from "./assets/gif/run.gif";
+import QQ from "./assets/gif/giphy.gif";
 
 function App() {
     const [output, setOutput] = useState();
+    const [isAlluploaded, setIsAlluploaded] = useState(false);
+    const compCanv = useRef([]);
+    useEffect(() => {
+        window.gifler(QQ).get((a) => {
+            const lastFrameIndex = a["_frames"].length - 1;
+            a["_frames"].forEach((frame, i) => {
+                const canvas = window.gifler.createBufferCanvas(
+                    frame,
+                    frame.width,
+                    frame.height
+                );
+                let resizedCanvas;
+
+                const img = new Image();
+                img.addEventListener(
+                    "load",
+                    () => {
+                        resizedCanvas = document.createElement("canvas");
+                        const MAX_WIDTH = 200;
+                        const scaleSize = MAX_WIDTH / frame.width;
+                        resizedCanvas.width = MAX_WIDTH;
+                        resizedCanvas.height = frame.height * scaleSize;
+
+                        const ctx = resizedCanvas.getContext("2d");
+                        ctx.drawImage(
+                            img,
+                            0,
+                            0,
+                            resizedCanvas.width,
+                            resizedCanvas.height
+                        );
+                        compCanv.current = [...compCanv.current, resizedCanvas];
+                        if (i === lastFrameIndex) {
+                            setIsAlluploaded(true);
+                        }
+                        document.body.append(resizedCanvas);
+                    },
+                    false
+                );
+                img.src = canvas.toDataURL();
+            });
+        });
+    }, []);
 
     useEffect(() => {
-        const gif = new GIF({
-            workers: 2,
-            workerScript: process.env.PUBLIC_URL + "/gif.worker.js",
-            quality: 10,
-        });
-
-        window.gifler(QQ).get((a) => {
-            const res = a["_frames"].map((frame) => {
-                const canvas = window.gifler.createBufferCanvas(frame, frame.width, frame.height);
-                document.body.append(canvas);
-                return canvas;
+        if (isAlluploaded) {
+            const gif = new GIF({
+                workers: 2,
+                workerScript: process.env.PUBLIC_URL + "/gif.worker.js",
+                quality: 10,
+                // background: "#fff",
+                transparent: 0x00000000
             });
 
             const exportCuts = async () => {
-                const cuts = res;
-
-                cuts.forEach((canvas) => {
-                    gif.addFrame(canvas, { delay: 100 });
+                const resizedCanvas = compCanv.current;
+                resizedCanvas.forEach((canvas) => {
+                    gif.addFrame(canvas, { delay: 150, copy: true });
                 });
 
                 gif.on("finished", async (blob) => {
-                    console.log("blob:")
-                    console.log(blob);
-                    console.log("---------------");
-
                     const url = URL.createObjectURL(blob);
+                    setOutput(url);
                     const file = new File([blob], "흑흑.gif", {
                         type: "image/gif",
                     });
-
-                    console.log("blob to file:")
-                    console.log(file);
-                    console.log("---------------");
-                    setOutput(url);
-
                     const formData = new FormData();
                     formData.append("image", file);
 
-                    // https://velog.io/@josworks27/formData-console.log
-                    for (let value of formData.values()) {
-                        console.log("formdata에 file이 제대로 담겼는지 확인:")
-                        console.log(value);
-                        console.log("---------------");
-                    }
-
-                    const requestUrl = "https://mandarin.api.weniv.co.kr/";
-
-                    const res = await fetch(requestUrl + "image/uploadfile", {
+                    const serverUrl = "https://mandarin.api.weniv.co.kr/";
+                    const res = await fetch(serverUrl + "image/uploadfile", {
                         method: "POST",
                         body: formData
                     });
-                    console.log("res:")
-                    console.log(res);
-                    console.log("---------------");
-
                     const json = await res.json();
-                    console.log("res.json:")
                     console.log(json);
                 });
 
@@ -72,9 +88,9 @@ function App() {
             };
 
             exportCuts();
-        });
-    }, []);
+        }
+    }, [isAlluploaded]);
 
-    return <img src={output} alt="rendering" />;
+    return <img style={{width: "480px", height: "480px", objectFit: "contain"}} src={output} alt="gif 재합성" />;
 }
 export default App;
